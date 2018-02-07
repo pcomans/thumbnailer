bcbcea3532d0cf8195c7521de3c87de26bf76819
Philipp Comans <philipp.comans@gmail.com>
2018-02-07 12:47:38 -0800

    Use port 8080

--- index.js
+++ index.js
@@ -34,4 +34,4 @@ app.get('/thumb/:vidUrlEnc', function (req, res) {
     });
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(8080, () => console.log('Example app listening on port 3000!'))
